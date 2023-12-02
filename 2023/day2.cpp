3bef16c6d19c34d039cfedf38a6c5a1aa02e7b49
Mateusz Glowka <mateusz.glowka90@gmail.com>
2023-12-02 15:43:25 +0100

    ++

---
+++ 2023/day2.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <iterator>
+#include <map>
+
+#include "common.hpp"
+
+int main() {
+  auto input = readFile("input/day2.txt");
+
+  std::cout << input.size() << std::endl;
+
+  return 0;
+}
