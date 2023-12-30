4e15366b26473390ffc2d001241ce4f838fa9ddd
headi90 <headi90@users.noreply.github.com>
2023-12-30 10:32:53 +0000

    day7 part2

--- 2023/day7.cpp
+++ 2023/day7.cpp
@@ -10,7 +10,7 @@
 using Hand = std::pair<std::string, long long>;
 using Hands = std::vector<Hand>;
 
-const std::map<char, long long> cards = {
+const std::map<char, long long> cardsVals1 = {
     {'2', 2},
     {'3', 3},
     {'4', 4},
@@ -26,13 +26,27 @@ const std::map<char, long long> cards = {
     {'A', 14},
 };
 
+const std::map<char, long long> cardsVals2 = {
+    {'J', 1},
+    {'2', 2},
+    {'3', 3},
+    {'4', 4},
+    {'5', 5},
+    {'6', 6},
+    {'7', 7},
+    {'8', 8},
+    {'9', 9},
+    {'T', 10},
+    {'Q', 11},
+    {'K', 12},
+    {'A', 13},
+};
+
 bool isAllCarsDifferent(const std::string &hand)
 {
   std::vector<char> cards{};
-  for (auto &c : hand)
-  {
-    cards.push_back(c);
-  }
+  std::copy(hand.begin(), hand.end(), std::back_inserter(cards));
+
   std::sort(cards.begin(), cards.end());
   auto it = std::unique(cards.begin(), cards.end());
   return it == cards.end();
@@ -41,10 +55,7 @@ bool isAllCarsDifferent(const std::string &hand)
 bool isPair(const std::string &hand)
 {
   std::vector<char> cards{};
-  for (auto &c : hand)
-  {
-    cards.push_back(c);
-  }
+  std::copy(hand.begin(), hand.end(), std::back_inserter(cards));
 
   std::sort(cards.begin(), cards.end());
 
@@ -61,10 +72,7 @@ bool isPair(const std::string &hand)
 bool isTwoPairs(const std::string &hand)
 {
   std::vector<char> cards{};
-  for (auto &c : hand)
-  {
-    cards.push_back(c);
-  }
+  std::copy(hand.begin(), hand.end(), std::back_inserter(cards));
 
   std::sort(cards.begin(), cards.end());
 
@@ -83,10 +91,7 @@ bool isTwoPairs(const std::string &hand)
 bool isThreeOfKind(const std::string &hand)
 {
   std::vector<char> cards{};
-  for (auto &c : hand)
-  {
-    cards.push_back(c);
-  }
+  std::copy(hand.begin(), hand.end(), std::back_inserter(cards));
 
   std::sort(cards.begin(), cards.end());
 
@@ -103,10 +108,7 @@ bool isThreeOfKind(const std::string &hand)
 bool isFullHouse(const std::string &hand)
 {
   std::vector<char> cards{};
-  for (auto &c : hand)
-  {
-    cards.push_back(c);
-  }
+  std::copy(hand.begin(), hand.end(), std::back_inserter(cards));
 
   std::sort(cards.begin(), cards.end());
 
@@ -124,10 +126,7 @@ bool isFullHouse(const std::string &hand)
 bool isFourOfKind(const std::string &hand)
 {
   std::vector<char> cards{};
-  for (auto &c : hand)
-  {
-    cards.push_back(c);
-  }
+  std::copy(hand.begin(), hand.end(), std::back_inserter(cards));
 
   std::sort(cards.begin(), cards.end());
 
@@ -144,10 +143,7 @@ bool isFourOfKind(const std::string &hand)
 bool isFiveOfKind(const std::string &hand)
 {
   std::vector<char> cards{};
-  for (auto &c : hand)
-  {
-    cards.push_back(c);
-  }
+  std::copy(hand.begin(), hand.end(), std::back_inserter(cards));
 
   std::sort(cards.begin(), cards.end());
 
@@ -161,19 +157,16 @@ bool isFiveOfKind(const std::string &hand)
   return false;
 }
 
-bool compareCards(const char &a, const char &b)
+bool compareCards(const char &a, const char &b, const std::map<char, long long> &cardsVals)
 {
-  return cards.at(a) < cards.at(b);
+  return cardsVals.at(a) < cardsVals.at(b);
 }
 
-//get hand value
-// 1. all cards are different
-// 2. pair
-// 3. two pairs
-// 4. three of kind
-// 5. full house
-// 6. four of kind
-// 7. five of kind
+bool compareCards2(const char &a, const char &b)
+{
+  return compareCards(a, b, cardsVals2);
+}
+
 int getHandValue(const std::string &hand)
 {
   if (isFiveOfKind(hand))
@@ -207,31 +200,93 @@ int getHandValue(const std::string &hand)
   return 0;
 }
 
-bool compareHands(const Hand &a, const Hand &b)
+bool compareHands1(const Hand &a, const Hand &b)
 {
-  Hand aHand = a;
-  Hand bHand = b;
-
-  auto aHandValue = getHandValue(aHand.first);
-  auto bHandValue = getHandValue(bHand.first);
-
-  // std::cout << aHand.first << " " << aHandValue << " " << bHand.first << " " << bHandValue << std::endl;
+  auto aHandValue = getHandValue(a.first);
+  auto bHandValue = getHandValue(b.first);
 
   if (aHandValue == bHandValue)
   {
-    for (auto i = 0; i < aHand.first.size(); i++)
+    for (auto i = 0; i < a.first.size(); i++)
     {
-      if (aHand.first[i] == bHand.first[i])
+      if (a.first[i] == b.first[i])
       {
         continue;
       }
-      return cards.at(aHand.first[i]) < cards.at(bHand.first[i]);
+      return compareCards(a.first[i], b.first[i], cardsVals1);
     }
   }
 
   return aHandValue < bHandValue;
-  
-  return false;
+}
+
+int countJokersOnHand(const std::string &hand)
+{
+ return std::count(hand.begin(), hand.end(), 'J');
+}
+
+char getMostCommonCard(const std::string &hand, int jokers)
+{
+  std::vector<char> cards{};
+  std::copy(hand.begin(), hand.end(), std::back_inserter(cards));
+
+  std::sort(cards.begin(), cards.end(), compareCards2);
+
+  int max = 0;
+  char maxCard = cards[jokers];
+  for(auto i = jokers; i < cards.size(); i++){
+    auto count = std::count(cards.begin(), cards.end(), cards[i]);
+    if(count > max){
+      max = count;
+      maxCard = cards[i];
+    }
+  }
+  return maxCard;
+}
+
+std::string modifyHandWithJokers(const std::string &hand)
+{
+  auto jokers = countJokersOnHand(hand);
+
+  if (jokers == 0 || isFiveOfKind(hand))
+  {
+    return hand;
+  }
+
+  std::vector<char> cards{};
+  std::copy(hand.begin(), hand.end(), std::back_inserter(cards));
+  std::sort(cards.begin(), cards.end(), compareCards2);
+
+  auto mostCommonCard = getMostCommonCard(hand, jokers);
+
+  for(auto i = 0; i < jokers; i++){
+    cards[i] = mostCommonCard;
+  }
+
+  return std::string(cards.begin(), cards.end());
+}
+
+bool compareHands2(const Hand &a, const Hand &b)
+{
+  auto aHand = modifyHandWithJokers(a.first);
+  auto bHand = modifyHandWithJokers(b.first);
+
+  auto aHandValue = getHandValue(aHand);
+  auto bHandValue = getHandValue(bHand);
+
+  if (aHandValue == bHandValue)
+  {
+    for (auto i = 0; i < a.first.size(); i++)
+    {
+      if (a.first[i] == b.first[i])
+      {
+        continue;
+      }
+      return compareCards(a.first[i], b.first[i], cardsVals2);
+    }
+  }
+
+  return aHandValue < bHandValue;
 }
 
 long solve1(std::vector<std::string> &input)
@@ -246,44 +301,38 @@ long solve1(std::vector<std::string> &input)
     hands.push_back(std::make_pair(cardsScore[0], score));
   }
 
-  // std::cout << "before sort" << std::endl; 
-  // for(auto &hand: hands){
-  //   std::cout << hand.first << " " << hand.second << std::endl;
-  // }
-  // std::cout << std::endl;
-
-  std::sort(hands.begin(), hands.end(), compareHands);
-
-  // std::cout << "\nafter sort" << std::endl;
-  // for(auto &hand: hands){
-  //   std::cout << hand.first << " " << hand.second << std::endl;
-  // }
-  // std::cout << std::endl;
-
-  // std::vector<std::string> hands1{
-  //     "23456", "22345", "23445", "22344", "77884", "65477", "74537", "AAA23", "23242",
-  //     "32323", "23232", "AAKKK", "KKAAA", "22222", "KKKKK", "AAAAA", "KKAKK", "23333", "33332", "33322", "33222", "22223", "22233", "22333", "23332", "23322", "22322"};
-
-  // for (auto &hand : hands1)
-  // {
-  //   std::cout << hand << " " << isAllCarsDifferent(hand) << " " << isPair(hand) << " " << isTwoPairs(hand) << " " << isThreeOfKind(hand) << " " << isFullHouse(hand) << " " << isFourOfKind(hand) << " " << isFiveOfKind(hand) << std::endl;
-  // }
+  std::sort(hands.begin(), hands.end(), compareHands1);
 
   for(auto i = 0; i < hands.size(); i++){
     auto v = (i+1) * hands[i].second;
     result += v;
-    std::cout << hands[i].first << " " << hands[i].second << " " << v << std::endl;
-    }
+  }
 
   return result;
 }
 
-int solve2(std::vector<std::string> &input)
+long solve2(std::vector<std::string> &input)
 {
   long result = 0;
+  Hands hands{};
+
+  for (auto &line : input)
+  {
+    auto cardsScore = split(line, ' ', false);
+    auto score = stringToNum<long long>(cardsScore[1]);
+    hands.push_back(std::make_pair(cardsScore[0], score));
+  }
+
+  std::sort(hands.begin(), hands.end(), compareHands2);
+
+  for(auto i = 0; i < hands.size(); i++){
+    auto v = (i+1) * hands[i].second;
+    result += v;
+  }
 
   return result;
 }
+
 
 int main(int argc, char **argv)
 {
