9e1860136ca33da7d6f7cf6d670da35b9daff92f
Mateusz Glowka <mateusz.glowka@nokia.com>
2024-10-26 22:06:59 +0200

    2022 day6 python

---
+++ 2022/python/day6.py
@@ -0,0 +1,26 @@
+import sys
+
+def fun(file_name):
+    try:
+        with open(file_name) as f:
+            line = f.read()
+            # print(line)
+        
+        for i in range(3,len(line)):
+            code = line[i-3:i+1]
+            s = set(code)
+            # print(f"{i}, {code} -> {s}")
+            if len(code) == len(s):
+                print(f"Found: {i+1}")
+                break
+
+    except FileNotFoundError:
+        print(f"File {file_name} not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python day1.py <file_name>")
+    else:
+        fun(sys.argv[1])
